3c4320aab664a63665c0d423b98c2b6ee207f8ff
haitheone123 <91556072+haitheone123@users.noreply.github.com>
2021-10-03 00:34:33 +0700

    version2 thay doi xoa phan tu

--- Xóa số âm/Xóa số âm.cpp
+++ Xóa số âm/Xóa số âm.cpp
@@ -24,7 +24,7 @@ void nhapMang(int n) {
 }
 int main() {
     int n;
-    cout << " Nhap vao so cac so cua phan tu " << endl;
+    cout << " Nhap vao so cac so cua phan tu - hello world " << endl;
     cin >> n;
     nhapMang(n);
     showlist(danhSachDiem);
